cb93a0a112c2b56c47fc376a30b4939683cfa20c
artshar <sharapudinov@mail.ru>
2017-03-19 15:45:15 +0300

    sale export fix merge

--- local/components/bacara/sale.export.1c/functions.php
+++ local/components/bacara/sale.export.1c/functions.php
@@ -1,5 +1,5 @@
 <?
-require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/sale/mysql/export.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/sale/mysql/export.php");
 
 class CSaleExport2XML
 {
